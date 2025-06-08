58dabf9bc6d0106ee83fb42097882174279b396d
Jodi <jodi08131111@gmail.com>
2025-06-09 00:32:59 +0800

    後臺調整

--- src/pages/GoHome/Follow.jsx
+++ src/pages/GoHome/Follow.jsx
@@ -45,18 +45,35 @@ function Follow() {
       <section id="follow">
         {/* 左耳 */}
         <div className="gohomeLeftEar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 210" fill="none">
-            <path d="M144.506 0.804903C200.79 1.01477 237.927 145.728 268.127 184.104C298.326 222.48 16.5937 214.301 1.65421 184.104C-13.2853 153.907 88.2215 0.595039 144.506 0.804903Z" fill="#fff" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="271"
+            height="210"
+            viewBox="0 0 271 210"
+            fill="none"
+          >
+            <path
+              d="M144.506 0.804903C200.79 1.01477 237.927 145.728 268.127 184.104C298.326 222.48 16.5937 214.301 1.65421 184.104C-13.2853 153.907 88.2215 0.595039 144.506 0.804903Z"
+              fill="#fff"
+            />
           </svg>
         </div>
 
         {/* 右耳 */}
         <div className="gohomeRightEar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 209" fill="none">
-            <path d="M136.287 0.805664C193.733 16.44 283.046 154.203 268.97 182.658C254.028 212.855 -27.7009 221.034 2.49843 182.658C24.868 154.232 51.045 67.4618 85.9896 24.293L130.526 43.4375L136.287 0.805664Z" fill="#fff" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="271"
+            height="209"
+            viewBox="0 0 271 209"
+            fill="none"
+          >
+            <path
+              d="M136.287 0.805664C193.733 16.44 283.046 154.203 268.97 182.658C254.028 212.855 -27.7009 221.034 2.49843 182.658C24.868 154.232 51.045 67.4618 85.9896 24.293L130.526 43.4375L136.287 0.805664Z"
+              fill="#fff"
+            />
           </svg>
         </div>
-
         <div className="gohomeBox followBox">
           <header className="followHeader">
             <div className="followCatName">
@@ -64,7 +81,9 @@ function Follow() {
             </div>
             <div className="followTitle">
               <span className="followTitle1">每月領養追蹤</span>
-              <span className="followTitle2">(請於每月底前完成上傳貓咪生活照)</span>
+              <span className="followTitle2">
+                (請於每月底前完成上傳貓咪生活照)
+              </span>
             </div>
             <div className="followCircles">
               <div className="circle gray">未上傳</div>
@@ -89,14 +108,28 @@ function Follow() {
       {showModal && (
         <div className="uploadModal">
           <div className="uploadBox">
-            <p>請上傳 {selectedMonth} 月的生活照片 3 張（須清楚拍攝貓咪正面）</p>
-            <input type="file" accept="image/*" multiple onChange={handleFileChange} />
+            <p>
+              請上傳 {selectedMonth} 月的生活照片 3 張（須清楚拍攝貓咪正面）
+            </p>
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={handleFileChange}
+            />
             <div className="previewBox">
               {previews.map((src, index) => (
-                <img key={index} src={src} alt={`預覽圖${index}`} className="previewImage" />
+                <img
+                  key={index}
+                  src={src}
+                  alt={`預覽圖${index}`}
+                  className="previewImage"
+                />
               ))}
             </div>
-            <button onClick={handleUpload} className="uploadBtn">上傳</button>
+            <button onClick={handleUpload} className="uploadBtn">
+              上傳
+            </button>
           </div>
         </div>
       )}
@@ -105,4 +138,3 @@ function Follow() {
 }
 
 export default Follow;
-
